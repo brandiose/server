3db1f00d5a2d8679d4216a3bb30b29bb8f882085
Sam Acuna <samueleishion@gmail.com>
2018-10-14 00:21:45 -0500

    #1 udpated app name in log file

--- utils/log.js
+++ utils/log.js
@@ -4,18 +4,18 @@ const error = chalk.red;
 const warning = chalk.keyword('orange');
 const info = chalk.magenta;
 const success = chalk.green;
-const app = chalk.cyan('SNDWAVE~');
+const app = chalk.cyan('brandiose~');
 
 module.exports = {
 	error: function(...message) {
 		this.custom(error('ERROR'), message);
-	}, 
+	},
 	warn: function(...message) {
 		this.custom(warning('WARN'), message);
-	}, 
+	},
 	info: function(...message) {
 		this.custom(info('INFO'), message);
-	}, 
+	},
 	success: function(...message) {
 		this.custom(success('SUCCESS'), message);
 	},
